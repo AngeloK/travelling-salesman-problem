a3154d4012df91404ee6c64d0c355873a2c65fc5
Neyanbhbin <yongjie.kanng@gmail.com>
2015-12-02 11:46:09 -0600

    add test performance

--- greedy_driver.py
+++ greedy_driver.py
@@ -11,8 +11,13 @@ class GreedyDriver(Driver):
     def __init__(self, start, adjacent_matrix):
         super(GreedyDriver, self).__init__(start, adjacent_matrix)
 
-
     def solve_tsp(self, start, remained_set):
+        '''Choose the vertex v such that the cost of (start ,v) is minimum.
+        The time complexity of his algorithm O(n^2). The computing of greedy
+        algorithm is very fast, but it can not make sure to create optimal
+        every time, it can only create suboptimal solution.
+        '''
+
         print "Solve using greedy algorithm..."
         print "------------------------------------"
         remained_set = remained_set - {start}
@@ -23,17 +28,16 @@ class GreedyDriver(Driver):
         while remained_set:
             min_cost = np.inf
             for vertex in remained_set:
-                if self.cost(vertex, path[-1])< min_cost:
+                if self.cost(vertex, path[-1]) < min_cost:
                     min_cost = self.cost(vertex, path[-1])
                     next_vertext = vertex
             total_min_cost += min_cost
             path.append(next_vertext)
             remained_set = remained_set - {next_vertext}
 
-        total_min_cost+=self.cost(start, path[-1])
+        total_min_cost += self.cost(start, path[-1])
         path.append(start)
         print total_min_cost
         print path
         print "End!"
         print "------------------------------------"
-
---
+++ test_performance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import helpers
+from dp_driver import DPDriver
+from brute_force_tsp import Brute_force_driver
+from greedy_driver import GreedyDriver
+import numpy as np
+import profile
