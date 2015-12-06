4023bdad8233303d4f01071ec3ab9d4ba921dae7
Neyanbhbin <yongjie.kanng@gmail.com>
2015-12-06 10:22:49 -0600

    add directed matrix and random path

--- helpers.py
+++ helpers.py
@@ -4,19 +4,21 @@
 import numpy as np
 
 
-def initialize_cities(map_size):
+def initialize_cities(map_size, is_directed=False):
     # Initialize a city map randomly and store data into an adjacent matrix.
     # Adjancent matrix is a symmetric matrix if the graph is an undirected
-    # graph.
+    # graph. if is_directed is equal to 'True', then this function will create
+    # a directed graph.
     cities_set = []
     init_maxtrix = np.random.random_integers(
         1, 100, size=(
             map_size, map_size))
-    symm_matrix = (init_maxtrix+init_maxtrix.T)/2
+    if not is_directed:
+        init_maxtrix = (init_maxtrix+init_maxtrix.T)/2
     for i in range(0, map_size):
         cities_set.append(i)
-        symm_matrix[i][i] = 0
-    return symm_matrix, cities_set
+        init_maxtrix[i][i] = 0
+    return init_maxtrix, cities_set
 
 
 def draw_map(adjacent_matrix):
